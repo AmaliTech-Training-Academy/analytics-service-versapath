beb57b7061291a0de2f5f2aac103b6402315390a
kevin <murindakevinmulk@gmail.com>
2025-10-12 15:47:32 +0200

    feat: create reponse dtos for capsules, atoms and clusters

---
+++ src/main/java/com/capstone/dto/response/ClusterSummaryDto.java
@@ -0,0 +1,21 @@
+package com.capstone.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ClusterSummaryDto {
+    private UUID id;
+    private UUID clusterId;
+    private String clusterName;
+    private String description;
+}
