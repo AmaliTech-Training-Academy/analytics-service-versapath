03475a0acf63077d3b4dfa7c0fe3564e31dfcbd5
kevin <murindakevinmulk@gmail.com>
2025-10-17 09:02:38 +0200

    feat: Add REST controller with separate endpoints for talent readiness filtering and analytics

---
+++ src/main/java/com/capstone/dto/request/ClusterFilterRequest.java
@@ -0,0 +1,15 @@
+package com.capstone.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClusterFilterRequest {
+    private List<UUID> clusterIds;
+}
